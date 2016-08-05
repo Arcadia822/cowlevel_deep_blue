1fff6f775d7d92428cf611b4043a2ed6fc4aa1e1
WeiHan <wei.han@renmai-inc.com>
2016-08-05 14:52:12 +0800

    update new theme

--- cowlevel_style.js
+++ cowlevel_style.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cowlevel Style
 // @namespace    http://www.zhouyuxin.net/
-// @version      1.5.2.3
+// @version      1.5.3
 // @description  CowLevel Steam Color Theme
 // @author       Arcadia
 // @match        https://*.cowlevel.net/*
@@ -40,28 +40,32 @@ function addGlobalStyle(css) {
         .layer {background: rgb("+MAIN_BG_COLOR+")}                                                                                         \
         .single-post {background: rgb("+MAIN_BG_COLOR+")}                                                                                   \
         .footer {background: rgb("+MAIN_BG_COLOR+")}                                                                                        \
-        .comments + .wrapper + .fat-footer {background: rgb("+SECOND_BG_COLOR+")}                                                           \
+        .comments.wrapper.fat-footer {background: rgb("+SECOND_BG_COLOR+")}                                                                 \
         .comments {background: rgb("+MAIN_BG_COLOR+")}                                                                                      \
         .btn-card-border{background: rgb("+SECOND_BG_COLOR+")}                                                                              \
         .tip-window{background-color: rgb("+MAIN_BG_COLOR+")}                                                                               \
-        .feedback-layer .layui-layer-title{ background: rgba("+SECOND_BG_COLOR+",.85);                                                      \
+        .feedback-layer.layui-layer-title{ background: rgba("+SECOND_BG_COLOR+",.85);                                                       \
                                             border-bottom: 1px solid rgba(255,255,255,.1);                                                  \
                                             color: rgba(255,255,255,.9)}                                                                    \
         .tip-tag{background: rgb("+SECOND_BG_COLOR+")}                                                                                      \
         .tip-game{background: rgb("+SECOND_BG_COLOR+")}                                                                                     \
-        .hot-box .cl-card{background: rgb("+MAIN_BG_COLOR+")}                                                                               \
+        .hot-box.cl-card{background: rgb("+SECOND_BG_COLOR+")}                                                                              \
+        .swiper-slide.cl-shadow.block{background: rgb("+SECOND_BG_COLOR+")}                                                                 \
         #search-result-box {background: rgb("+SECOND_BG_COLOR+")}                                                                           \
-        .wanguo-swiper .shouchang-swiper .swiper-slide {background: rgb("+SECOND_BG_COLOR+")}                                               \
+        .wanguo-swiper.shouchang-swiper.swiper-slide {background: rgb("+SECOND_BG_COLOR+")}                                                 \
         .vm-feed-card-article {background: rgb("+SECOND_BG_COLOR+")}                                                                        \
         .vm-feed-card-answer {background: rgb("+SECOND_BG_COLOR+")}                                                                         \
         .vm-feed-card-reivew {background: rgb("+SECOND_BG_COLOR+")}                                                                         \
         .vm-feed-card-question {background: rgb("+SECOND_BG_COLOR+")}                                                                       \
         .vm-feed-card-game {background: rgb("+SECOND_BG_COLOR+")}                                                                           \
-        div:not(.tip-window) .gl-user-card-box {background: -webkit-gradient(linear, 0 24%, 0 100%, from(rgba("+MAIN_BG_COLOR+",.1)), to(rgba("+MAIN_BG_COLOR+",1)));  \
+        div:not(.tip-window) .gl-user-card-box {background: -webkit-gradient(linear, 0 24%, 0 100%, from(rgba("+MAIN_BG_COLOR+",.1)),       \
+                     to(rgba("+MAIN_BG_COLOR+",1)));                                                                                        \
                      background: linear-gradient(-180deg, rgba("+MAIN_BG_COLOR+",.1) 24%, rgba("+MAIN_BG_COLOR+",1) 100%);                  \
                      background: -moz-linear-gradient(top, rgba("+MAIN_BG_COLOR+".1),rgba("+MAIN_BG_COLOR+",1))}                            \
-        div.vm-personal-card div.clearfix:not(.gl-user-card-box) {background: rgb("+MAIN_BG_COLOR+")}                                                                           \
+        div.vm-personal-card + div.clearfix:not(.gl-user-card-box) {background: rgb("+MAIN_BG_COLOR+")}                                     \
         .vm-style-double-bevel {background: rgb("+MAIN_BG_COLOR+"); padding-top:3px}                                                        \
+        .vm-layer-more-user {background: rgb("+MAIN_BG_COLOR+")}                                                                            \
+        section.vm-avatar.flex-box {background: rgb("+SECOND_BG_COLOR+")}                                                                   \
         .left-bevel {display: none}                                                                                                         \
         .right-bevel {display: none}                                                                                                        \
         .wangEditor-container {background: rgb("+SECOND_BG_COLOR+")}                                                                        \
