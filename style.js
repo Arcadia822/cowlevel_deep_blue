72c6729037132e733ecdb7fb40add181585e7ee2
hanwei <wei.han@renmai-inc.com>
2017-03-23 17:14:19 +0800

    update version

--- style.js
+++ style.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cowlevel Deep Blue
 // @namespace    Arc
-// @version      1.7.1
+// @version      1.7.2
 // @description  A Solarized like dark theme for Cowlevel.net
 // @author       Arcadia
 // @match        https://*.cowlevel.net/*
